20627fbdefae3101be67fbf5480fc7a50aac8635
quin <queenshafaraniabilqis@gmail.com>
2024-10-28 13:43:11 +0700

    Need some revision

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A55A6",
+      },
+    },
+  },
+  plugins: [],
+};
